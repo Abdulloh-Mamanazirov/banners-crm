d4f9796e9919312a035ad6109ef7d663a82447c4
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-06-05 14:46:41 +0500

    export new pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,19 @@ import { Banners, Create } from "./Banners";
 import Home from "./Home";
 import Admins from "./Admins";
 import Orders from "./Orders";
+import OrderID from "./Orders/ID";
+import OrderTable from "./Orders/ID/Table";
 import NotFound from "./404";
 
 export * from "./Finance";
-export { Login, Home, Admins, Banners, Create, Orders, NotFound };
+export {
+  Login,
+  Home,
+  Admins,
+  Banners,
+  Create,
+  Orders,
+  OrderID,
+  OrderTable,
+  NotFound,
+};
