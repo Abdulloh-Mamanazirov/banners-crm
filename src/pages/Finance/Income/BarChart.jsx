caaa3669d30882dd3b4e18f564912ac5e62fef84
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-06-10 11:26:06 +0500

    add onclick event to bars to open a modal

--- src/pages/Finance/Income/BarChart.jsx
+++ src/pages/Finance/Income/BarChart.jsx
@@ -1,8 +1,11 @@
+import { useRef, useState } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart } from "chart.js/auto";
 
 const index = ({ monthly, title, color }) => {
-  // monthly = monthly.sort((a, b) => a.month - b.month);
+  const chartRef = useRef();
+  const modalRef = useRef();
+  const [modalData, setModalData] = useState();
 
   function getMonth(month) {
     switch (String(month)) {
@@ -56,25 +59,84 @@ const index = ({ monthly, title, color }) => {
   };
 
   return (
-    <div className="shadow-xl rounded-2xl p-1 border border-gray-400 mt-3 max-w-[98%] mx-auto">
-      <h3 className="text-center text-xl font-semibold mb-2">
-        Oylik {title} statistikasi:
-      </h3>
-      <Bar
-        data={monthly_data}
-        options={{
-          responsive: true,
-          scales: {
-            x: {
-              stacked: true,
+    <>
+      <div className="shadow-xl rounded-2xl p-1 border border-gray-400 mt-3 max-w-[98%] mx-auto">
+        <h3 className="text-center text-xl font-semibold mb-2">
+          Oylik {title} statistikasi:
+        </h3>
+        <Bar
+          data={monthly_data}
+          options={{
+            responsive: true,
+            scales: {
+              x: {
+                stacked: true,
+              },
+              y: {
+                stacked: true,
+              },
             },
-            y: {
-              stacked: true,
+            onClick: (event) => {
+              const elements = chartRef.current.getElementsAtEventForMode(
+                event,
+                "nearest",
+                { intersect: true },
+                true
+              );
+              if (elements.length) {
+                const index = elements[0].index;
+                // const datasetIndex = elements[0].datasetIndex;
+                // const label = monthly_data.datasets[datasetIndex].label;
+                // const value = monthly_data.datasets[datasetIndex].data[index];
+                modalRef.current.showModal();
+                setModalData(index);
+              }
             },
-          },
-        }}
-      />
-    </div>
+          }}
+          ref={chartRef}
+        />
+      </div>
+
+      {/* modal */}
+      <dialog
+        ref={modalRef}
+        className="bg-white p-3 border rounded-lg backdrop:bg-black/30 md:w-1/3"
+      >
+        <div className="flex items-center justify-between gap-3 mb-4">
+          <h3 className="font-medium text-lg">{getMonth(modalData + 1)} oyi</h3>
+          <button
+            onClick={() => {
+              setModalData(null);
+              modalRef.current.close();
+            }}
+            type="button"
+            className="w-8 h-8 border rounded-full hover:bg-gray-50"
+          >
+            <span className="fa-solid fa-close" />
+          </button>
+        </div>
+        <div className="flex flex-col gap-2">
+          <table className="text-center">
+            <thead>
+              <tr>
+                <th className="border p-2">Kompaniya</th>
+                <th className="border p-2">Summa</th>
+              </tr>
+            </thead>
+            <tbody>
+              {monthly?.hh?.[modalData]?.map?.((item, ind) => (
+                <tr key={ind}>
+                  <td className="border p-1">{item?.label}</td>
+                  <td className="border p-1">
+                    {Number(item?.data).toLocaleString("uz-Uz")}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </dialog>
+    </>
   );
 };
 
